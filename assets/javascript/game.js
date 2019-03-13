2a548d6362b711e2806133ceeb96f453aecfb171
Corey Yates <coreyyates@Coreys-MacBook-Pro.local>
2019-03-13 14:11:16 -0400

    pseudocoded

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+funtion startGame () {
+    // pick a random number between 1 and 100
+
+    // set a number value for each crystal
+
+    // create click event for each crystal
+
+    // create function that will add the value of crystals
+
+    //if/else statement if current player value equals target number, you win.
+                // would love to create a win screen with animation like fireworks
+                //have to have the game reset and record the win
+    // if the number goes over you lose.
+                //have to reset the game and record the lose.
+           
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
