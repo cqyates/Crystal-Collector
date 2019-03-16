93a3d562bd96febe0c4bc964d33a0e03ac021459
Corey Yates <coreyyates@Coreys-MacBook-Pro.local>
2019-03-16 19:08:21 -0400

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,54 @@
 $( document ).ready(function() {
 
-    var wins = 0;
-    var losses = 0;
-    var totalGames = 0;
-    var gemTotal = 0;
-    
-
-    //Set a computer guess
     var targetNumber = Math.floor((Math.random()*120)+19);
-
-    //show Target Number in HTML
     $("#target-number").text(targetNumber);
 
-    // set a number value for each crystal
     var valueGem1 = Math.floor((Math.random()*12)+1);
     var valueGem2 = Math.floor((Math.random()*12)+1);
     var valueGem3 = Math.floor((Math.random()*12)+1);
     var valueGem4 = Math.floor((Math.random()*12)+1);
+
+    var wins = 0;
+    var losses = 0;
+    var gemTotal = 0;
     
-    //confirm working values for computer guess and gem
-    console.log("the target number is " + targetNumber)
-    console.log("gem 1 is worth " + valueGem1)
-    console.log("gem 2 is worth " + valueGem2)
-    console.log("gem 3 is worth " + valueGem3)
-    console.log("gem 4 is worth " + valueGem4)
+
+    $("#wins").text("Wins:" + wins);
+    $("#losses").text("Losses: " + losses);
+
+    function reset() {
+        var targetNumber = Math.floor((Math.random()*120)+19);
+        $("#target-number").text(targetNumber);
+        gemTotal = 0;
+        $("#gem-total").text(gemTotal);
+    }
+        //function runs when you win, resets the game
+    function win(){
+        wins++;
+        alert ("You Win!");
+        $("#wins").text("Wins:" + wins);
+        reset();
+    }
+        //function that runs when you lose, resets the game
+    function lose(){
+        losses++;
+        alert ("You Lose");
+        $("#losses").text("Losses: " + losses);
+        reset();
+    }
+    
+    
+   
 
     // create click event for each crystal
+
     $("#gem-1").on("click", function() {
         gemTotal = gemTotal + valueGem1;
         $("#gem-total").text(gemTotal);
         if (targetNumber === gemTotal) {
-            wins++
-            $("#wins").text("Wins:" + wins);
-            totalGames++
-            $("#total-games").text("Total Games:" + totalGames);
+           win();
         } else if (targetNumber < gemTotal) {
-            losses++
-            $("#losses").text("Losses: " + losses);
-            totalGames++
-            $("#total-games").text("Total Games:" + totalGames);
+        lose();
         }
     });
 
@@ -46,15 +56,9 @@ $( document ).ready(function() {
         gemTotal = gemTotal + valueGem2;
         $("#gem-total").text(gemTotal);
         if (targetNumber === gemTotal) {
-            wins++
-            $("#wins").text("Wins:" + wins)
-            totalGames++
-            $("#total-games").text("Total Games:" + totalGames);
+           win();
         } else if (targetNumber < gemTotal) {
-            losses++
-            $("#losses").text("Losses:" + losses);
-            totalGames++
-            $("#total-games").text("Total Games:" + totalGames);
+            lose();
         }
     });
 
@@ -62,15 +66,9 @@ $( document ).ready(function() {
         gemTotal = gemTotal + valueGem3;
         $("#gem-total").text(gemTotal);
         if (targetNumber === gemTotal) {
-            wins++
-            $("#wins").text("Wins:" + wins);
-            totalGames++
-            $("#total-games").text("Total Games:" + totalGames);
+            win();
         } else if (targetNumber < gemTotal) {
-            losses++
-            $("#losses").text("Losses:" + losses);
-            totalGames++
-            $("#total-games").text("Total Games:" + totalGames);
+            lose();
         }
     });
 
@@ -78,21 +76,12 @@ $( document ).ready(function() {
         gemTotal = gemTotal + valueGem4;
         $("#gem-total").text(gemTotal);
         if (targetNumber === gemTotal) {
-            wins++
-            $("#wins").text("Wins:" + wins);
-            totalGames++
-            $("#total-games").text("Total Games:" + totalGames);
+           win();
         } else if (targetNumber < gemTotal) {
-            losses++
-            $("#losses").text("Losses:" + losses);
-            totalGames++
-            $("#total-games").text("Total Games:" + totalGames);
+            lose();
         }
     });
-
     
-    //have to have the game reset and record the win
-    // if the number goes over you lose.
-                //have to reset the game and record the loss.
-                
+    
+
 });
